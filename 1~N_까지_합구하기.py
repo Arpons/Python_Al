60207b0db6f2a2a6836718a37a6b51e4fc65b8d8
GUBeng <ahs9702@daum.net>
2019-06-07 14:09:13 +0900

    O(n) 과 O(1) 추가

--- 1~N_까지_합구하기.py
+++ 1~N_까지_합구하기.py
@@ -1,10 +1,10 @@
-def total(n):
+def total(n):                       # O(1) : 필요한 계산 횟수가 입력 크기n 과 무관할때 
     k =  (n * (n + 1)) / 2
     return k
 
-def sum(n):
+def sum(n):                         # O(n) : 필요한 계산 횟수가 입력 크기n 과 비례할때
     s = 0
-    for i in range(1, n + 1):    # for 범위 (1부터 n+1까지)
+    for i in range(1, n + 1):       # for 범위 (1부터 n+1까지)
         s = s + i
     return s
 
