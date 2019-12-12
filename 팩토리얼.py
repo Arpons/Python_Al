c6e2f7ba8f817a3cf6358ee3c5250ffcd2e6e4ad
GUBeng <ahs9702@daum.net>
2019-12-12 15:49:12 +0900

    팩토리얼 구하기

--- 팩토리얼.py
+++ 팩토리얼.py
@@ -8,9 +8,16 @@ def fact2(n):
          return 1
      return n * fact2(n - 1)
 
+def fact3(n):
+    if n<= 1:
+        return 1
+    return n * fact3(n - 1)
+
 print(fact1(3))
 print(fact1(5))
 print(fact1(6))
 print(fact2(3))
 print(fact2(5))
 print(fact2(6))
+print(fact3(1))
+print(fact3(3))
